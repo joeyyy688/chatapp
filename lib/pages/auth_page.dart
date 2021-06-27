2d36e81a089b45c5b411ac332cbd599bc113ffaa
joeyyy68 <70595863+joeyyy68@users.noreply.github.com>
2021-06-27 13:20:19 +0000

    added firebase auth package and wired it up

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,5 +1,8 @@
 import 'package:chatapp/widgets/authForm.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class AuthPage extends StatefulWidget {
   const AuthPage({Key? key}) : super(key: key);
@@ -9,9 +12,58 @@ class AuthPage extends StatefulWidget {
 }
 
 class _AuthPageState extends State<AuthPage> {
+  final _auth = FirebaseAuth.instance;
+  var isLoading = false;
+
+  Future<void> _submitAuthForm(
+      String email, String password, String username, bool isLogin) async {
+    UserCredential authResult;
+    try {
+      setState(() {
+        isLoading = true;
+      });
+      if (isLogin) {
+        authResult = await _auth.signInWithEmailAndPassword(
+            email: email, password: password);
+      } else {
+        authResult = await _auth.createUserWithEmailAndPassword(
+            email: email, password: password);
+
+        await FirebaseFirestore.instance
+            .collection('users')
+            .doc(authResult.user!.uid)
+            .set({'username': username, 'email': email});
+      }
+    } on PlatformException catch (e) {
+      setState(() {
+        isLoading = false;
+      });
+      var errorMessage = "An Error Occured, please check your credentials";
+
+      if (e.message != null) {
+        errorMessage = e.message!;
+      }
+
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text(errorMessage),
+        backgroundColor: Theme.of(context).errorColor,
+      ));
+    } catch (error) {
+      setState(() {
+        isLoading = false;
+      });
+      print(error);
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text(error.toString().substring(37)),
+        backgroundColor: Theme.of(context).errorColor,
+      ));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Theme.of(context).primaryColor, body: AuthForm());
+        backgroundColor: Theme.of(context).primaryColor,
+        body: AuthForm(_submitAuthForm, isLoading));
   }
 }
--- lib/widgets/authForm.dart
+++ lib/widgets/authForm.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/material.dart';
 
 class AuthForm extends StatefulWidget {
-  const AuthForm({Key? key}) : super(key: key);
+  AuthForm(this.submitFn, this.isLoading);
+  final void Function(
+      String email, String password, String userName, bool isLogin) submitFn;
+  final isLoading;
 
   @override
   _AuthFormState createState() => _AuthFormState();
 }
 
 class _AuthFormState extends State<AuthForm> {
-  bool _passwordVisibile = false;
+  bool _passwordVisibile = true;
+  final _formKey = GlobalKey<FormState>();
+  var _isLogin = true;
+  String userEmail = '';
+  String userName = '';
+  String userPassword = '';
+
+  void trySubmit() {
+    final isValid = _formKey.currentState!.validate();
+
+    FocusScope.of(context).unfocus();
+    if (isValid) {
+      _formKey.currentState!.save();
+    }
+
+    widget.submitFn(
+        userEmail.trim(), userPassword.trim(), userName.trim(), _isLogin);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -18,24 +39,53 @@ class _AuthFormState extends State<AuthForm> {
           child: Padding(
             padding: EdgeInsets.all(16),
             child: Form(
+              key: _formKey,
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
-                  TextFormField(
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                      labelText: 'Email',
-                      //labelStyle: TextStyle(fontWeight: FontWeight.bold)
+                  if (!_isLogin)
+                    TextFormField(
+                      key: ValueKey('email'),
+                      validator: (value) {
+                        if (value!.isEmpty || !value.contains('@')) {
+                          return 'Please Enter a Valid E-mail';
+                        }
+                        return null;
+                      },
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        //labelStyle: TextStyle(fontWeight: FontWeight.bold)
+                      ),
+                      onSaved: (newValue) {
+                        userEmail = newValue!;
+                      },
                     ),
-                  ),
                   TextFormField(
+                    key: ValueKey('name'),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Name field cannot be empty';
+                      }
+                      return null;
+                    },
                     keyboardType: TextInputType.name,
                     decoration: InputDecoration(
                       labelText: 'UserName',
                       //labelStyle: TextStyle(fontWeight: FontWeight.bold)
                     ),
+                    onSaved: (newValue) {
+                      userName = newValue!;
+                    },
                   ),
                   TextFormField(
+                    key: ValueKey('password'),
+                    validator: (value) {
+                      if (value!.isEmpty || value.length < 7) {
+                        return 'Password must be at least 7 characters long';
+                      }
+                      return null;
+                    },
                     keyboardType: TextInputType.name,
                     obscureText: _passwordVisibile,
                     decoration: InputDecoration(
@@ -55,23 +105,44 @@ class _AuthFormState extends State<AuthForm> {
                         )
                         //labelStyle: TextStyle(fontWeight: FontWeight.bold)
                         ),
+                    onSaved: (newValue) {
+                      userPassword = newValue!;
+                    },
                   ),
                   SizedBox(
                     height: 12,
                   ),
-                  ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20))),
-                      onPressed: () {},
-                      child: Text('Login')),
-                  TextButton(
-                      style: TextButton.styleFrom(
-                          primary: Theme.of(context).primaryColor),
-                      onPressed: () {},
-                      child: Text(
-                        'Create New Account',
-                      ))
+                  if (widget.isLoading) ...{
+                    Center(child: CircularProgressIndicator()),
+                  } else ...{
+                    Container(
+                      child: Column(
+                        children: [
+                          ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(20))),
+                              onPressed: () {
+                                trySubmit();
+                              },
+                              child: Text(_isLogin ? 'Login' : 'Sign Up')),
+                          TextButton(
+                              style: TextButton.styleFrom(
+                                  primary: Theme.of(context).primaryColor),
+                              onPressed: () {
+                                setState(() {
+                                  _isLogin = !_isLogin;
+                                });
+                              },
+                              child: Text(
+                                _isLogin
+                                    ? 'Create New Account'
+                                    : 'I already have an account',
+                              ))
+                        ],
+                      ),
+                    )
+                  }
                 ],
               ),
             ),
